708a9cc168f01767b0f7a757b7306c8d8fb2e9a5
Alay Naru <narualay030@gmail.com>
2021-08-30 20:39:18 +0530

    ..

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const UNIVERSAL = {
-  // BASEURL: "https://mywayblog.herokuapp.com",
-  BASEURL: "http://localhost:5000",
+  BASEURL: "https://mywayblog.herokuapp.com",
+  // BASEURL: "http://localhost:5000",
 };
 
 export default UNIVERSAL;
